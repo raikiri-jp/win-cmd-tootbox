7cd7be6f5fdea280d20fd0bbfffafb083fa16ba4
Miyahara Yuuki <59301668+raikiri-jp@users.noreply.github.com>
2025-07-06 14:45:12 +0900

    Add legacy and Windows 11 context menu scripts

---
+++ win/context-classic.cmd
@@ -0,0 +1,10 @@
+@echo off
+whoami /priv | find "SeDebugPrivilege" > nul
+if %errorlevel% neq 0 (
+  @powershell start-process %~0 -verb runas
+  exit
+)
+
+reg add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
+taskkill /f /im explorer.exe
+start explorer.exe
